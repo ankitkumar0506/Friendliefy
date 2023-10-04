28d9803b3ae612bac1e9b4ed4dd46eb43c013357
unknown <shreyanshshah2912@gmail.com>
2023-10-04 14:58:19 +0530

    hello

---
+++ Friendliefy/resources/views/welcome.blade.php
@@ -0,0 +1,5 @@
+<div class="flex items-center justify-end mt-4">
+    <a href="{{ url('authorized/google') }}">
+        <img src="https://developers.google.com/identity/images/btn_google_signin_dark_normal_web.png" style="margin-left: 3em;">
+    </a>
+</div>
